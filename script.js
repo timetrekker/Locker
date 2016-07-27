c79586f31d905ec67ba2493f714df20a1e21b123
timetrekker <abey.holmes7@gmail.com>
2016-07-27 22:57:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,6 @@
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
-
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
